23be3146c2e3eaa909ad661ce68985e72e9ae4d8
JP <jpmagday21@gmail.com>
2025-05-09 09:51:11 +0800

    Structure 2

--- OneDrive/Desktop/Grp 2-Struct_Act/struct2.c
+++ OneDrive/Desktop/Grp 2-Struct_Act/struct2.c
@@ -11,47 +11,17 @@ struct Seat {
     int isAssigned; // 0 = available, 1 = taken
 };
 
-// Function to initialize seats
-void initializeSeats(struct Seat seats[ROWS][COLUMNS]) {
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLUMNS; ++j) {
-            seats[i][j].row = i + 1;
-            seats[i][j].letter = 'A' + j;
-            seats[i][j].isAssigned = 0;
-        }
-    }
-}
+void initializeSeats(struct Seat seats[ROWS][COLUMNS]);
 
-// Function to display current seat map
-void displaySeats(struct Seat seats[ROWS][COLUMNS]) {
-    printf("\nCurrent Seat Map:\n\n");
-    for (int i = 0; i < ROWS; ++i) {
-        printf("%d\t", i + 1);
-        for (int j = 0; j < COLUMNS; ++j) {
-            if (seats[i][j].isAssigned)
-                printf("X\t");
-            else
-                printf("%c\t", seats[i][j].letter);
-        }
-        printf("\n");
-    }
-}
+void displaySeats(struct Seat seats[ROWS][COLUMNS]);
 
-// Function to check if all seats are assigned
-int allSeatsAssigned(struct Seat seats[ROWS][COLUMNS]) {
-    for (int i = 0; i < ROWS; ++i)
-        for (int j = 0; j < COLUMNS; ++j)
-            if (!seats[i][j].isAssigned)
-                return 0; // Found one available
-    return 1; // All are assigned
-}
+int allSeatsAssigned(struct Seat seats[ROWS][COLUMNS]);
 
 int main() {
     struct Seat seats[ROWS][COLUMNS];
-    char input[4];
-    int rowIndex, colIndex;
-    char letter;
-    int row;
+    char input[4], letter;
+    int rowIndex, colIndex, row;
+    
     initializeSeats(seats);
 
     printf("Welcome to the Airplane Seat Reservation System!\n");
@@ -89,4 +59,39 @@ int main() {
     }
 
     return 0;
+}
+
+// Function to initialize seats  
+void initializeSeats(struct Seat seats[ROWS][COLUMNS]) {
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLUMNS; ++j) {
+            seats[i][j].row = i + 1;
+            seats[i][j].letter = 'A' + j;
+            seats[i][j].isAssigned = 0;
+        }
+    }
+}
+
+// Function to display current seat map
+void displaySeats(struct Seat seats[ROWS][COLUMNS]) {
+    printf("\nCurrent Seat Map:\n\n");
+    for (int i = 0; i < ROWS; ++i) {
+        printf("%d\t", i + 1);
+        for (int j = 0; j < COLUMNS; ++j) {
+            if (seats[i][j].isAssigned)
+                printf("X\t");
+            else
+                printf("%c\t", seats[i][j].letter);
+        }
+        printf("\n");
+    }
+}
+
+// Function to check if all seats are assigned
+int allSeatsAssigned(struct Seat seats[ROWS][COLUMNS]) {
+    for (int i = 0; i < ROWS; ++i)
+        for (int j = 0; j < COLUMNS; ++j)
+            if (!seats[i][j].isAssigned)
+                return 0; // Found one available
+    return 1; // All are assigned
 }
